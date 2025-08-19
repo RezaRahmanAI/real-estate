a8313f5fd9f038582887ab454c013051fabbb718
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2025-08-19 19:01:58 +0600

    Footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FadeInDirective } from '../../directives/fade-in.directive';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FadeInDirective],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {}
+export class FooterComponent  {
+  
+}
