abc9ecaf225393a3c71fdd6d3987f9b8298f3979
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2025-08-29 23:21:21 +0600

    animation service added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { ProjectExploreComponent } from '../../components/project-explore/project-explore.components';
+import { AfterViewInit, Component } from '@angular/core';
 import { VisionBannerComponent } from '../../components/vision-banner/vision-banner.component';
 import { TestimonialCarouselComponent } from '../../components/testimonial/testimonial.component';
 import { ProjectSlideComponent } from '../../components/hero-slide/project-slide.component';
 import { FollowUpdateComponent } from '../../components/follow-update/follow-update.component';
 import { SliderComponent } from '../../components/slider/slider.component';
+import { ProjectExploreComponent } from '../../components/project-explore/project-explore.components';
+import { AnimationService } from '../../services/animation.service';
+
 
 @Component({
   selector: 'app-home',
@@ -18,6 +20,12 @@ import { SliderComponent } from '../../components/slider/slider.component';
     SliderComponent,
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+  constructor(private anim: AnimationService) {}
+
+  ngAfterViewInit() {
+    this.anim.animateOnScroll('.fade-up');
+    this.anim.animateOnScroll('.zoom-in');
+  }
+}
