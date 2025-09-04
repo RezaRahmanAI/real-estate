86c7f9902d043cc932ff7078824780be56cdff85
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2025-09-04 10:52:59 +0600

    running

--- src/app/directives/navbar-scroll.directive.ts
+++ src/app/directives/navbar-scroll.directive.ts
@@ -10,7 +10,6 @@ export class NavbarScrollDirective implements OnInit {
   private lastScroll = 0;
   private scrollUpDelta = 0;
   private scrollDownDelta = 0;
-  private revealThreshold = 80;
   private hideThreshold = 80;
 
   constructor(private el: ElementRef, private lenisService: LenisService) {}
@@ -20,34 +19,20 @@ export class NavbarScrollDirective implements OnInit {
 
     this.lenisService.onScroll((scroll: number) => {
       if (scroll <= 20) {
-        nav.classList.remove('glass', 'hide');
-        nav.classList.add('transparent');
-        this.scrollUpDelta = 0;
-        this.scrollDownDelta = 0;
+        nav.classList.remove('hide-navbar');
       } else {
         if (scroll > this.lastScroll) {
           this.scrollDownDelta += scroll - this.lastScroll;
           this.scrollUpDelta = 0;
-
           if (this.scrollDownDelta > this.hideThreshold) {
-            nav.classList.add('hide');
-            nav.classList.remove('glass', 'transparent');
+            nav.classList.add('hide-navbar');
             this.scrollDownDelta = 0;
           }
-        } else {
-          this.scrollUpDelta += this.lastScroll - scroll;
-          this.scrollDownDelta = 0;
-
-          if (this.scrollUpDelta > this.revealThreshold) {
-            nav.classList.remove('hide', 'transparent');
-            nav.classList.add('glass');
-            this.scrollUpDelta = 0;
-          }
-        }
+        } 
       }
-
       this.lastScroll = scroll;
     });
   }
 }
+
 
